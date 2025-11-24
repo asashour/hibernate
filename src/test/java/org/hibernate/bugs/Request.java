aafa2a63832b5f81a1ce9c71c99c72cea80053f1
Ahmed Ashour <asashour@yahoo.com>
2025-11-24 11:41:15 +0100

    Fix class.

--- src/test/java/org/hibernate/bugs/Request.java
+++ src/test/java/org/hibernate/bugs/Request.java
@@ -1,10 +1,8 @@
 package org.hibernate.bugs;
 
 import jakarta.persistence.*;
-import org.hibernate.annotations.SQLRestriction;
 
 @Entity
-@SQLRestriction("tampered = false")
 public abstract class Request {
 
 
